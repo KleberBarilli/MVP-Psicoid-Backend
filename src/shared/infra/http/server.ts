77d49046de6d7bc9c9c346aee28d032e0f9ddb9b
kleberbarilli <kleber.barilli73@gmail.com>
2022-07-09 12:55:00 -0300

    app.ts configs and error handlers

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,28 @@
+import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
-app.listen(3333, () => {
+app.use(cors);
+app.use(express.json());
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+	if (error instanceof AppError) {
+		return res.status(error.statusCode).json({
+			status: 'error',
+			message: error.message,
+		});
+	}
+	return res.status(500).json({
+		status: 'error',
+		message: error.message,
+	});
+});
+
+app.listen(process.env.PORT, () => {
+	// eslint-disable-next-line no-console
 	console.log(`Rodando na porta ${process.env.PORT}`);
 });
