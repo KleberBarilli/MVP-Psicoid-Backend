1e0f8442caec1aa7dda05af2cb83a97f037c6b81
kleber.barilli <kleber.barilli73@gmail.com>
2022-08-09 21:48:26 -0300

    refactor: shared moduels

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,9 @@
 import { container } from 'tsyringe';
-import { IClientsRepository } from '../../modules/clients/domain/repositories/IClientsRepository';
-import ClientsRepository from '../../modules/clients/infra/prisma/repositories/ClientsRepository';
-import '../../modules/auth/providers';
-import PsychologistsRepository from '../../modules/psico/infra/prisma/repositories/PsychologistsRepository';
-import { IPsychologistsRepository } from '../../modules/psico/domain/repositories/IPsychologistsRepository';
+import { IClientsRepository } from '@modules/clients/domain/repositories/IClientsRepository';
+import ClientsRepository from '@modules/clients/infra/prisma/repositories/ClientsRepository';
+import '@modules/auth/providers';
+import PsychologistsRepository from '@modules/psico/infra/prisma/repositories/PsychologistsRepository';
+import { IPsychologistsRepository } from '@modules/psico/domain/repositories/IPsychologistsRepository';
 
 container.registerSingleton<IClientsRepository>(
 	'ClientsRepository',
