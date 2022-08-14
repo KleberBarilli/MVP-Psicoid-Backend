624343bfe0bd9b3dc88b7e9647bd1c6229afe72b
kleber.barilli <kleber.barilli73@gmail.com>
2022-08-13 23:24:33 -0300

    fix: cpf/cnpj validator

--- src/shared/utils/validators/Company.ts
+++ src/shared/utils/validators/Company.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
-
+import { cnpj } from 'cpf-cnpj-validator';
 interface ICompanyForm {
 	cnpj: string;
 	name: string;
@@ -10,7 +10,13 @@ export const validateCompany = (company: ICompanyForm) =>
 	yup
 		.object()
 		.shape({
-			cnpj: yup.string().typeError('CNPJ Inválido').required(),
+			cnpj: yup
+				.string()
+				.test({
+					test: v => !v || cnpj.isValid(v),
+					message: 'CNPJ Inválido',
+				})
+				.required('O CNPJ é obrigatório'),
 			name: yup.string().required('O nome fantasia é obrigatório'),
 			tradingName: yup.string().required('A razão social é obrigatória'),
 		})
--- src/shared/utils/validators/Identity.ts
+++ src/shared/utils/validators/Identity.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
-
+import { cpf } from 'cpf-cnpj-validator';
 interface IIdentityForm {
 	firstName: string;
 	lastName: string;
@@ -12,6 +12,12 @@ export const validateIdentity = (identity: IIdentityForm) =>
 		.shape({
 			firstName: yup.string().required('O nome é obrigatório'),
 			lastName: yup.string().required('O sobrenome é obrigatório'),
-			cpf: yup.string().typeError('CPF Inválido').required(),
+			cpf: yup
+				.string()
+				.test({
+					test: v => !v || cpf.isValid(v),
+					message: 'CPF Inválido',
+				})
+				.required('O CNPJ é obrigatório'),
 		})
 		.validate(identity, { abortEarly: false, stripUnknown: true });
