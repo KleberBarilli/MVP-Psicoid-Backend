5d8edc24e3ab769ed06e7ae1af868280e229e646
kleber.barilli <kleber.barilli73@gmail.com>
2022-08-25 20:37:03 -0300

    recebendo o email no controller

--- src/modules/auth/infra/http/controllers/ResetPasswordController.ts
+++ src/modules/auth/infra/http/controllers/ResetPasswordController.ts
@@ -4,12 +4,12 @@ import ResetPasswordService from '@modules/auth/services/ResetPasswordService';
 
 export default class ResetPasswordController {
 	public async create(req: Request, res: Response): Promise<Response> {
-		const { password, token } = req.body;
+		const { email, password, token } = req.body;
 
 		const resetPassword = container.resolve(ResetPasswordService);
 
 		await resetPassword.execute({
-			userId: req.user.id,
+			email,
 			password,
 			token,
 		});
