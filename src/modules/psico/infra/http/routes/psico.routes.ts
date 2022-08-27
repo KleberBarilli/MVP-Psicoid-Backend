c7371b8f102c7e63c0e8bac7b79b76e8952b16eb
kleber.barilli <kleber.barilli73@gmail.com>
2022-08-26 21:33:16 -0300

    feat: show psico

--- src/modules/psico/infra/http/routes/psico.routes.ts
+++ src/modules/psico/infra/http/routes/psico.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import PsychologistController from '../controllers/PsychologistController';
+import ShowPsychologistController from '../controllers/ShowPsychologistController';
 
 const psicoRouter = Router();
 const psicoController = new PsychologistController();
+const showPsicoController = new ShowPsychologistController();
 
 psicoRouter.post('/', psicoController.create);
+psicoRouter.get('/:id', showPsicoController.show);
 
 export default psicoRouter;
