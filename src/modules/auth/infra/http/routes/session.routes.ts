2ad78569222df33aadcc016611ae961cf834f379
kleber.barilli <kleber.barilli73@gmail.com>
2022-08-28 19:23:56 -0300

    feat: roleSelector

--- src/modules/auth/infra/http/routes/session.routes.ts
+++ src/modules/auth/infra/http/routes/session.routes.ts
@@ -1,5 +1,6 @@
 import isAuthenticated from "@shared/infra/http/middlewares/isAuthenticated";
 import { Router } from "express";
+import { handleRole } from "@shared/infra/http/middlewares/handleRole";
 
 import SessionController from "../controllers/SessionController";
 import WhoiamController from "../controllers/WhoiamController";
