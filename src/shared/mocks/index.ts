a21c2fe7afc98e0b7f5f8c62984b8a233c3ab9dc
kleber.barilli <kleber.barilli73@gmail.com>
2022-08-28 15:30:59 -0300

    e2e config

---
+++ src/shared/mocks/index.ts
@@ -0,0 +1,23 @@
+export const pacient = {
+	credentials: {
+		email: "test@example.com",
+		password: "pass12346",
+		role: "PACIENT",
+	},
+	identity: {
+		firstName: "John",
+		lastName: "Douglas",
+		cpf: "79521782021",
+	},
+	contact: {
+		cellPhone: "54-99532383",
+	},
+	address: {
+		zipCode: "99990-000",
+		street: "main avenue",
+		neighborhood: "center",
+		number: "54",
+		city: "Passo Fundo",
+		state: "RS",
+	},
+};
