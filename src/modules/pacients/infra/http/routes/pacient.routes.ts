2dce84940084f4f3e7c58c48f4b92c72b568fcda
kleber.barilli <kleber.barilli73@gmail.com>
2022-09-03 14:30:55 -0300

    update pacient route

--- src/modules/pacients/infra/http/routes/pacient.routes.ts
+++ src/modules/pacients/infra/http/routes/pacient.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import CreatePacientController from "../controllers/CreatePacientController";
 import ShowPacientController from "../controllers/ShowPacientController";
-
+import UpdatePacientController from "../controllers/UpdatePacientController";
 const pacientRouter = Router();
 const createPacientController = new CreatePacientController();
 const showPacient = new ShowPacientController();
+const updatePacient = new UpdatePacientController();
 
 pacientRouter.post("/", createPacientController.create);
 pacientRouter.get("/:id", showPacient.show);
+pacientRouter.put("/:id", updatePacient.update);
 
 export default pacientRouter;
