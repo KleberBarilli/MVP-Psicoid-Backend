acba2171124b6995902c3b04655bac11e08ce022
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-08 15:07:48 -0300

    return avg rating

--- src/modules/psico/services/ShowPsychologistService.ts
+++ src/modules/psico/services/ShowPsychologistService.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from "tsyringe";
 import { IPsychologistsRepository } from "../domain/repositories/IPsychologistsRepository";
 import { getKmDistance } from "@shared/utils/distance";
+import { IReview } from "@shared/interfaces/IReview";
+import { arrAvg } from "@shared/utils/etc";
 @injectable()
 export default class ShowPsychologistService {
 	constructor(
@@ -18,6 +20,12 @@ export default class ShowPsychologistService {
 			},
 		);
 
-		return [psico, distance];
+		let ratings: number[] = [];
+		psico?.reviews.map((review: IReview) => {
+			ratings.push(review.rating);
+		});
+
+		const avgRating = Math.round(arrAvg(ratings));
+		return [psico, distance, avgRating];
 	}
 }
