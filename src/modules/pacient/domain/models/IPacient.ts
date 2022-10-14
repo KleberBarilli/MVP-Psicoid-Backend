cc71df797573ab9c34b6712176f200af97331857
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-14 14:08:09 -0300

    types guest pacient

--- src/modules/pacient/domain/models/IPacient.ts
+++ src/modules/pacient/domain/models/IPacient.ts
@@ -1,7 +1,8 @@
 export interface IPacient {
 	id: string;
-	credentialId: string;
-	profileId: string;
+	credentialId: string | null;
+	profileId: string | null;
+	guestId: string | null;
 	selectedPsychologistId: string | null;
 	createdAt: Date;
 	updatedAt: Date;
