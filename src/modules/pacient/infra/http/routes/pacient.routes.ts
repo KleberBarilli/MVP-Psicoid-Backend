895d5f4c356ae8744d1173f3d4fb0ed3a98eff8c
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-14 11:41:13 -0300

    list pacients routes and handle roles

--- src/modules/pacient/infra/http/routes/pacient.routes.ts
+++ src/modules/pacient/infra/http/routes/pacient.routes.ts
@@ -1,15 +1,33 @@
+import { handleRole } from "@shared/infra/http/middlewares/handleRole";
 import isAuthenticated from "@shared/infra/http/middlewares/isAuthenticated";
 import { Router } from "express";
 import AddPsychologistController from "../controllers/AddPsychologistController";
 import CreatePacientController from "../controllers/CreatePacientController";
+import ListPacientsByPsicoController from "../controllers/ListPacientsByPsicoController";
 import ShowPacientController from "../controllers/ShowPacientController";
 import UpdatePacientController from "../controllers/UpdatePacientController";
 
 const pacientRouter = Router();
 
 pacientRouter.post("/", new CreatePacientController().handle);
-pacientRouter.get("/:id", new ShowPacientController().handle);
+pacientRouter.post(
+	"/add-psychologist",
+	isAuthenticated,
+	handleRole("PACIENT"),
+	new AddPsychologistController().handle,
+);
 pacientRouter.put("/", isAuthenticated, new UpdatePacientController().handle);
-pacientRouter.post("/add-psychologist", isAuthenticated, new AddPsychologistController().handle);
+pacientRouter.get(
+	"/:id",
+	isAuthenticated,
+	handleRole("PSYCHOLOGIST"),
+	new ShowPacientController().handle,
+);
+pacientRouter.get(
+	"/",
+	isAuthenticated,
+	handleRole("PSYCHOLOGIST"),
+	new ListPacientsByPsicoController().handle,
+);
 
 export default pacientRouter;
