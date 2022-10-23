0769a1dfdb6d034dc27343215eccd03b4914e9bd
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-22 21:57:04 -0300

    fix create notificacao

--- src/modules/notification/services/CreateNotificationService.ts
+++ src/modules/notification/services/CreateNotificationService.ts
@@ -3,7 +3,9 @@ import prisma from "@shared/prisma";
 
 export default class CreateNotificationService {
 	public static async execute({ type, data, views }: any): Promise<Notification> {
-		const notification = await prisma.notification.create({ data: { type, data, views } });
+		const notification = await prisma.notification.create({
+			data: { type, data, views: { createMany: { data: views } } },
+		});
 		//    emitSocketEvent('notify:send', notificacao)
 		return notification;
 	}
