0943e9c2dadb7c86bf6aa6b468433c2279ee46fe
kleber.barilli <kleber.barilli73@gmail.com>
2022-11-05 17:16:27 -0300

    add admin

---
+++ src/modules/admin/domain/models/ICreateAdmin.ts
@@ -0,0 +1,7 @@
+import { ICredential } from '@shared/interfaces/ICredential'
+import { IProfile } from '@shared/interfaces/IProfile'
+
+export interface ICreateAdmin {
+	credential: ICredential
+	profile: IProfile
+}
