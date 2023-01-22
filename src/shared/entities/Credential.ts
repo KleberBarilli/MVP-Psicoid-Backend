4659e04a5bdd82b985d8d6cf86aaf364549e8f46
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-22 11:06:09 -0300

    fixes

--- src/shared/entities/Credential.ts
+++ src/shared/entities/Credential.ts
@@ -2,12 +2,15 @@ import { Credential, Provider, Role } from "@prisma/client";
 
 export class CredentialEntity implements Credential {
 	id: bigint;
+	integrationId: string;
 	provider: Provider;
 	email: string;
 	password: string;
 	tokenRecovery: string | null;
 	role: Role;
-	inactive: boolean;
 	createdAt: Date;
 	updatedAt: Date;
+	inactivatedAt: Date | null;
+	lastLoginAt: Date | null;
+	lastLoginIp: string | null;
 }
