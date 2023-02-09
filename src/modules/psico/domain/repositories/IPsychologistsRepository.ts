39f244563dc2b6c63802ce94800f181304421ea9
kleber.barilli <kleber.barilli73@gmail.com>
2023-02-09 19:00:45 -0300

    fix

--- src/modules/psico/domain/repositories/IPsychologistsRepository.ts
+++ src/modules/psico/domain/repositories/IPsychologistsRepository.ts
@@ -4,7 +4,7 @@ import {
 	IPsychologistShortUpdate,
 } from "../models/IPsychologist";
 import { PsychologistEntity } from "../../infra/prisma/entities/Psychologist";
-import { IUpdatePsychologist } from "../models/IUpdatedPsychologist";
+import { IUpdatedPsychologist } from "../models/IUpdatedPsychologist";
 import { ITherapeuticApproaches } from "@shared/interfaces/IApproaches";
 import { ICreateInvite } from "../models/ICreateInvite";
 import { CreateInviteResponse } from "@shared/interfaces/types/psico.types";
@@ -13,7 +13,7 @@ export interface IPsychologistsRepository {
 	create(data: ICreatePsychologist): Promise<PsychologistEntity>;
 	findById(integrationId: string): Promise<IPsychologist | null>;
 	findAll(pagination: any): Promise<[number, any]>;
-	update(data: IUpdatePsychologist): Promise<PsychologistEntity>;
+	update(data: IUpdatedPsychologist): Promise<PsychologistEntity>;
 	findAllApproaches(pagination: any): Promise<ITherapeuticApproaches[]>;
 	findOneApproach(id: number): Promise<ITherapeuticApproaches | null>;
 	addApproach(id: number, psicoId: number): Promise<IPsychologistShortUpdate>;
